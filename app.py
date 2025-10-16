e309b05acd17ce213be77ff32f0135bf4797450d
vijayk1707 <vijaykrishnan1704@gmail.com>
2025-10-17 02:43:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,9 @@ def predict():
 
 # --------------------------- Run server
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use Render’s dynamic port
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+
 
 
